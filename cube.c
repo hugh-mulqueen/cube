6f49f13e07ad5517adf93617e76e00d0eb7e6df8
Hugh Mulqueen <hughberto@gmail.com>
2013-04-27 00:07:50 +0200

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,105 @@
+//This piece of software is licenced under the terms of the GPL version 3.
+
+//(C) Hugh Mulqueen 2013
+
+#include <stdio.h>
+
+#include <math.h>
+
+#define pi = "3.142857143"
+
+int x;
+
+double pi;
+
+double A;
+
+double B;
+
+double C;
+
+double D;
+
+double E;
+
+double F;
+
+double cubed;
+
+int main()
+
+{
+
+int G, M;
+
+int G1, G2;
+
+double a=x^0;
+
+double b=x^1;
+
+double c=x^2;
+
+double d=x^3;
+
+double e=x^4;
+
+double f=x^5;
+
+double g=x^6;
+
+double h=x^7;
+
+double i=x^8;
+
+double j=x^9;
+
+double k=x^10;
+
+double l=x^11;
+
+double m=x^12;
+
+double n=x^13;
+
+double o=x^14;
+
+double p=x^15;
+
+A=a*b*c*d*e*f*g*h*i*j*k*l*m*n*n*o*p;
+
+B=a*b*c*d*e*f*g*h*i*j*k*l*m*n*n*o*p;
+
+C=a*b*c*d*e*f*g*h*i*j*k*l*m*n*n*o*p;
+
+D=a*b*c*d*e*f*g*h*i*j*k*l*m*n*n*o*p;
+
+E=a*b*c*d*e*f*g*h*i*j*k*l*m*n*n*o*p;
+
+F=a*b*c*d*e*f*g*h*i*j*k*l*m*n*n*o*p;
+
+cube();
+
+{
+
+G1=m*9.81^2;
+
+128*x^64/sqrt(2)*M*9.81^2=G2;
+
+for (x=0; x < 128; x++)
+
+printf("Well Enter value for x", x);
+
+scanf("%d", &x);
+
+printf("Please enter the value for mass", M);
+
+scanf("%d", &M);
+
+printf("The resulting number is:", cubed);
+
+}
+if x=-1 printf("RESULT");
+else x printf("cube number", cubed);
+
+}
